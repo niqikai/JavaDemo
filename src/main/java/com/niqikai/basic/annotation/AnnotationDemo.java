c7e4bc26991de2dae979fa231456a85b10ff166d
niqikai <niqikai@aliyun.com>
2018-09-28 00:25:24 +0800

    delete some code

--- src/main/java/com/niqikai/basic/annotation/AnnotationDemo.java
+++ src/main/java/com/niqikai/basic/annotation/AnnotationDemo.java
@@ -1,42 +1,42 @@
 
-package com.niqikai.basic.annotation;
+// package com.niqikai.basic.annotation;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
+// import java.lang.annotation.Annotation;
+// import java.lang.reflect.Field;
 
-/**
- * Annotation Demo
- */
-public class AnnotationDemo {
-
-
-    public static void main(String[] args) {
-        try {   
-            Class clazz = Class.forName("com.niqikai.basic.annotation.Student");
-
-            //获得类的所有有效注解
-            Annotation[] annotations =clazz.getAnnotations();
-            for (Annotation a : annotations) {
-                System.out.println(a);
-            }
-            //获得类的指定的注解
-            Table table = (Table) clazz.getAnnotation(Table.class);
-            System.out.println(table.value());
-            //获得类的属性的注解
-            Field field = clazz.getDeclaredField("name");
-            MyFiled myFiled = field.getAnnotation(MyFiled.class);
-            System.out.println(myFiled.column() + "--" + myFiled.type() + "--" + myFiled.length());
+// /**
+//  * Annotation Demo
+//  */
+// public class AnnotationDemo {
 
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+//     public static void main(String[] args) {
+//         try {   
+//             Class clazz = Class.forName("com.niqikai.basic.annotation.Student");
+
+//             //获得类的所有有效注解
+//             Annotation[] annotations =clazz.getAnnotations();
+//             for (Annotation a : annotations) {
+//                 System.out.println(a);
+//             }
+//             //获得类的指定的注解
+//             Table table = (Table) clazz.getAnnotation(Table.class);
+//             System.out.println(table.value());
+//             //获得类的属性的注解
+//             Field field = clazz.getDeclaredField("name");
+//             MyFiled myFiled = field.getAnnotation(MyFiled.class);
+//             System.out.println(myFiled.column() + "--" + myFiled.type() + "--" + myFiled.length());
+
+
+//         } catch (Exception e) {
+//             e.printStackTrace();
+//         }
 
 
 
 
-    }
+//     }
 
    
      
-}
+// }
